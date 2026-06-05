6b17782ede127966b654753021ca8a2e575143d1
dotsky <contact@sathishjayapal.me>
2026-06-04 22:33:21 -0500

    fix: qualify yup types in custom declarations

--- src/main/webapp/types/custom.d.ts
+++ src/main/webapp/types/custom.d.ts
@@ -1,13 +1,19 @@
 import * as yup from 'yup';
 
 declare module 'yup' {
-  interface StringSchema<TType extends yup.Maybe<string> = string|undefined, TContext = yup.AnyObject, TDefault = undefined, TFlags extends yup.Flags = ''> extends yup.Schema<TType, TContext, TDefault, TFlags> {
-    emptyToNull(msg?: yup.Message): yup.StringSchema<TType|null, TContext, TDefault, TFlags>;
-    numeric(precision: number | Reference<number>, scale: number | Reference<number>, msg?: Message<{ precision: number; scale: number; }>): this;
+  interface StringSchema<TType extends yup.Maybe<string> = string | undefined, TContext = yup.AnyObject, TDefault = undefined, TFlags extends yup.Flags = ''>
+    extends yup.Schema<TType, TContext, TDefault, TFlags> {
+    emptyToNull(msg?: yup.Message): yup.StringSchema<TType | null, TContext, TDefault, TFlags>;
+    numeric(
+      precision: number | yup.Reference<number>,
+      scale: number | yup.Reference<number>,
+      msg?: yup.Message<{ precision: number; scale: number }>,
+    ): this;
     offsetDateTime(msg?: yup.Message): this;
   }
-  interface NumberSchema<TType extends yup.Maybe<number> = number|undefined, TContext = yup.AnyObject, TDefault = undefined, TFlags extends yup.Flags = ''> extends yup.Schema<TType, TContext, TDefault, TFlags> {
-    emptyToNull(msg?: yup.Message): yup.NumberSchema<TType|null, TContext, TDefault, TFlags>;
+  interface NumberSchema<TType extends yup.Maybe<number> = number | undefined, TContext = yup.AnyObject, TDefault = undefined, TFlags extends yup.Flags = ''>
+    extends yup.Schema<TType, TContext, TDefault, TFlags> {
+    emptyToNull(msg?: yup.Message): yup.NumberSchema<TType | null, TContext, TDefault, TFlags>;
   }
 }
 
