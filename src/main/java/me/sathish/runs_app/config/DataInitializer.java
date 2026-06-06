555a4ce62417c5422cc28ac813b596effceee6f2
Sathish Jayapal <sathishjayapal@users.noreply.github.com>
2026-06-06 18:38:33 -0500

    feat: add profile condition to DataInitializer for non-test environments

--- src/main/java/me/sathish/runs_app/config/DataInitializer.java
+++ src/main/java/me/sathish/runs_app/config/DataInitializer.java
@@ -12,11 +12,13 @@ import me.sathish.runs_app.strava_run.StravaRunRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 
 @Configuration
+@Profile("!test")
 @Slf4j
 public class DataInitializer {
 
